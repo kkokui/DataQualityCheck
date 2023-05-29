718dd06dd150e7ed28a9ea8a79c5b40645b3a705
kkokui <18s2014b@student.gs.chiba-u.jp>
2023-05-29 15:50:21 +0900

    Fix usage.

--- plot_sigmaPar.cpp
+++ plot_sigmaPar.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		printf("usage: %s pos_res/sigmaPar_BeforeAlign.root\n", argv[0]);
+		printf("usage: %s pos_res/sigmaPar_BeforeAlign.root title\n", argv[0]);
 		return 0;
 	}
 	TString filename = argv[1];
