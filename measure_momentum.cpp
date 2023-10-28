a212dbcbcf9b7d4df7bd1da3fd636753ad426e80
kkokui <18s2014b@student.gs.chiba-u.jp>
2023-10-28 21:37:19 +0900

    specialized in momentum.

--- measure_momentum.cpp
+++ measure_momentum.cpp
@@ -1,28 +1,41 @@
-#include <stdio.h>
-
 #include <FnuMomCoord.hpp>
-#include <FnuDivideAlign.h>
-#include <FnuQualityCheck.h>
 
-// #include <EdbDataSet.h>
-
-// #include <TString.h>
+void CalcAllTrackMomentum(TObjArray *tracks,TString title)
+{
+    FnuMomCoord mom;
+    // printf("Momentum calculation started\n");
+    mom.ReadParFile("/home/kokui/LEPP/FASERnu/Tools/FnuMomCoord/par/Data_up_to_100plates_mod1.txt");
+    // TCanvas *c = new TCanvas();
+    // c->Print("test.pdf[");
+    TH1D hmom("hmom", "momentum", 100, 0, 7000);
+    int ntrk = tracks->GetEntriesFast();
+    for (int itrk = 0; itrk < ntrk; itrk++)
+    {
+        EdbTrackP *t = (EdbTrackP *)tracks->At(itrk);
+        double p = mom.CalcMomentum(t);
+        // mom.DrawMomGraphCoord(t,c,"test");
+        hmom.Fill(p);
+        if(0==itrk%1000)
+        {
+            printf("%3d%% done\r",itrk*100/ntrk);
+            fflush(stdout);
+        }
+    }
+    // c->Print("test.pdf]");
+    printf("100%% done\n", ntrk, ntrk);
+    mom.WriteRootFile("momentum_output/nt_"+title);
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc < 8)
+    if (argc < 4)
     {
-        printf("usage: ./align_and_measure_momentum linked_tracks.root title cut Xcenter Ycenter binwidth robustfactor\n");
+        printf("usage: ./align_and_measure_momentum linked_tracks.root title cut\n");
         return 1;
     }
     TString filename_linked_tracks = argv[1];
     TString title = argv[2];
     TString cut = argv[3];
-    double Xcenter, Ycenter, binWidth, robustFactor;
-    sscanf(argv[4],"%lf",&Xcenter);
-    sscanf(argv[5],"%lf",&Ycenter);
-    sscanf(argv[6],"%lf",&binWidth);
-    sscanf(argv[7],"%lf",&robustFactor);
     EdbDataProc *dproc = new EdbDataProc;
     EdbPVRec *pvr = new EdbPVRec;
     dproc->ReadTracksTree(*pvr, filename_linked_tracks, cut);
@@ -32,26 +45,6 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
-    FnuDivideAlign align;
-    align.SetRobustFactor(robustFactor);
-    align.SetBinWidth(binWidth);
-    // align.Align(tracks, Xcenter, Ycenter, pvr->Npatterns());
 
-    // FnuMomCoord mom;
-    // mom.ReadParFile("~/LEPP/FASERnu/Tools/FnuMomCoord/par/Data_up_to_200plates.txt");
-    // TH1D hmom("hmom", "momentum", 100, 0, 7000);
-    // for (int itrk = 0; itrk < ntrk; itrk++)
-    // {
-    //     EdbTrackP *t = (EdbTrackP *)tracks->At(itrk);
-    //     double p = mom.CalcMomentum(t);
-    //     hmom.Fill(p);
-    // }
-    // TCanvas c;
-    // hmom.Draw();
-    // c.Print("momentum_no_align.pdf");
-    FnuQualityCheck qc(pvr,"akirec");
-    qc.CalcEfficiency();
-    // qc.CalcDeltaXY(Xcenter,Ycenter,binWidth);
-    // qc.FitDeltaXY();
-    // qc.PlotPosRes("posres_no_align");
+    CalcAllTrackMomentum(tracks,title);
 }
